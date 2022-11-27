fd8e2bc0ea207c7fb4b3ea4a9a177885c8f30d80
dominicod <dominicodonnell99@gmail.com>
2022-11-27 13:36:16 -0600

    Feat: finish basic app structure, allowing you to decrypt and encrypt

--- Program.cs
+++ Program.cs
@@ -6,18 +6,39 @@ namespace Enigma_CSharp
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Would you like to encrypt(0) a message, or decrypt(1) a message:");
+            var userResponse = Console.ReadLine();
+
+            switch (userResponse)
+            {
+                case "0":
+                    Encrypt();
+                    return;
+                case "1":
+                    Decrypt();
+                    return;
+                default:
+                    Console.WriteLine("Error: Incorrect argument, can either be 0, or 1");
+                    return;
+            }
         }
 
-        public static void Encrypt(string[] args)
+        private static void Encrypt()
         {
-            var message = Encryptable.Encrypt(args[0], Int16.Parse(args[1]));
+            Console.WriteLine("Please enter a message(string), followed by a key(number):");
+            var userResponse = Console.ReadLine().Split(" ");
+            
+            var message = Encryptable.Encrypt(userResponse[0], Int16.Parse(userResponse[1]));
             Console.WriteLine($"Encrypted Message: {message.Encryption}, Original Message: {message.OriginalMessage}");
         }
-
-        public static void Decrypt(string[] args)
+        
+        private static void Decrypt()
         {
+            Console.WriteLine("Please enter the encrypted message(string), followed by the key used(number):");
+            var userResponse = Console.ReadLine().Split(" ");
             
+            var message = Decryptable.Decrypt(userResponse[0], Int16.Parse(userResponse[1]));
+            Console.WriteLine($"Encrypted Message: {message.Encryption}, Original Message: {message.Decryption}");
         }
     }
 }
